6e95a07cc561ac3df1d60956d13538682fcd769e
Minh Triet Khuong <khuongminhtriet@gmail.com>
2026-04-23 00:28:05 +1000

    update thumbnails pics

---
+++ app/src/main/java/com/example/a5_1c/data/Story.kt
@@ -0,0 +1,9 @@
+package com.example.a5_1c.data
+
+data class Story(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val imageRes: Int,
+    val relatedStoryIds: List<Int>
+)
